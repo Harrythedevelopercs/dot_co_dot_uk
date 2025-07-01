a8b1fef8a3dc229af9bbf161bfb7cd63a2242747
devsahal-crystallite <dev.ahs@productions-dev.com>
2025-07-01 06:42:33 +0500

    New Fix

--- src/components/website/home/published-books/slider/slider.jsx
+++ src/components/website/home/published-books/slider/slider.jsx
@@ -1,21 +1,12 @@
 // Components
 import { AutoPlaySlider } from "@/components"
 // Media
-<<<<<<< HEAD
-import One from "media/home/published-books/1.jpg"
-import Two from "media/home/published-books/2.jpg"
-import Three from "media/home/published-books/3.jpg"
-import Four from "media/home/published-books/4.jpg"
-import Five from "media/home/published-books/5.jpg"
-import Six from "media/home/published-books/6.jpg"
-=======
 import One from "media/home/published-books/1.png"
 import Two from "media/home/published-books/2.png"
 import Three from "media/home/published-books/3.png"
 import Four from "media/home/published-books/4.png"
 import Five from "media/home/published-books/5.png"
 import Six from "media/home/published-books/6.png"
->>>>>>> 0aba169c4111b11dc65d2ec3b8e913b90e206b6b
 // Next
 import Image from "next/image"
 // Css
