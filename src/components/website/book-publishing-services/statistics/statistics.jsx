7c84455208a4078f512ba50900749cf2005ecbf6
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-07-06 00:34:19 +0500

    new changes

--- src/components/website/book-publishing-services/statistics/statistics.jsx
+++ src/components/website/book-publishing-services/statistics/statistics.jsx
@@ -1,7 +1,7 @@
 // Next
 import Image from "next/image"
 // Media
-import Img from "media/book-launch-campaigns/statistics.png"
+import Img from "media/book-launch-campaigns/stats3.jpg"
 // Components
 import { CTAGroupLight } from "@/components"
 // Css
